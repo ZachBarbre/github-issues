04d2ef33595428e14985c15623f0c66761c7184b
Zach Barbre <zach.barbre@gmail.com>
2020-03-30 13:03:36 -0400

    markdown added. trying to get the <code> format down

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './IssueItem.css'
+import ReactMarkdown from 'react-markdown';
 
 function Issue(props) {
     return (
         <div className='issueCard'>
             <h3>{props.title}</h3>
             <a href={props.url}>Link to Issue on GitHub</a>
-            <p>{props.body}</p>
+            <div><ReactMarkdown source={props.body} /></div>
         </div>
     );
 }
